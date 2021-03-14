c805cb87c6043a8f57ceb479f3905cd7e811208a
KishanUtkarsh <kishan.utkarsh12082002@gmail.com>
2021-03-14 13:45:49 +0530

    adding more shutdown commands.

--- system_shutdown_restart.py
+++ system_shutdown_restart.py
@@ -1,11 +1,26 @@
 import os
 import sys
 class system_shutdown_restart:
-    def __init__(self, shutdown,restart,abort_shutdown):
+    def __init__(self, shutdown,restart,abort_shutdown,hibernate):
         shutdown.self  = shutdown
         restart.self  = restart
         abort_shutdown.self = abort_shutdown
+        hibernate.self = hibernate
 
-    def shutdown(True):
+    def shutdown(self , shutdown = True):
+        os.getcwd("C:\WINDOWS\system32>")
+        os.system("shutdown /s",shell = True)
+
+    def restart(self , restart = True ):
+        os.getcwd("C:\WINDOWS\system32>")
+        os.system("shutdown /r", shell = True)
+
+    def abort_shutdown(self,abourt_shutdown = True):
+        os.getcwd("C:\WINDOWS\system32>")
+        os.system("shutdown /a", shell=True)
+
+    def hibernate(self,hibernate = True):
+        os.getcwd("C:\WINDOWS\system32>")
+        os.system("shutdown /h", shell=True)
 
 
