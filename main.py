bd1321093ac9f30266d56e12f77858329e54d78b
KishanUtkarsh <kishan.utkarsh12082002@gmail.com>
2021-03-20 16:23:36 +0530

    adding more features.

--- main.py
+++ main.py
@@ -2,6 +2,21 @@ import webbrowser as wb
 import speech_recognition as sr
 import datetime
 import system_shutdown_restart as ssr
+import pyttsx3 as py
+
+engine = py.init()
+rate = engine.getProperty('rate')
+engine.setProperty('rate', 150)   # setting up speed of the voice
+voices = engine.getProperty('voices')
+engine.setProperty('voice',voices[1].id)  # setting up girls voice for boys voice remove 1 and put 0.
+volume = engine.getProperty('volume')
+engine.setProperty('volume',1.0) # setting volume 0 to 1
+
+
+def offline_sound(text):
+    engine.say(text)
+    engine.runAndWait()
+    engine.stop()
 
 
 A = sr.Recognizer()
@@ -12,54 +27,54 @@ def VoiceRecognition(x):
         A1 = sr.Recognizer()
         url = "https://www.youtube.com/results?search_query="
         with sr.Microphone() as  source:
-            print("speak video name you looking for:")
+            offline_sound("speak video name you looking for")
             audio2 = A1.listen(source)
             try:
                 qurrie = A1.recognize_google(audio2)
                 print(qurrie)
-                wb.get().open_new(url+qurrie)
+                wb.get().open_new(url + qurrie)
 
             except:
                 print("Speak properly!!!!!")
 
     elif 'Wikipedia' in x:
         A1 = sr.Recognizer()
-        url="https://en.wikipedia.org/wiki/"
+        url = "https://en.wikipedia.org/wiki/"
         with sr.Microphone() as  source:
             print("speak your qurries:")
             audio2 = A1.listen(source)
             try:
                 qurrie = A1.recognize_google(audio2)
                 print(qurrie)
-                wb.get().open_new(url+qurrie)
+                wb.get().open_new(url + qurrie)
 
             except:
                 print("Speak properly!!!!!")
 
     elif 'Python package' in x:
         A1 = sr.Recognizer()
-        url="https://pypi.org/search/?q="
+        url = "https://pypi.org/search/?q="
         with sr.Microphone() as  source:
             print("speak your module name:")
             audio2 = A1.listen(source)
             try:
                 qurrie = A1.recognize_google(audio2)
                 print(qurrie)
-                wb.get().open_new(url+qurrie)
+                wb.get().open_new(url + qurrie)
 
             except:
                 print("Speak properly!!!!!")
 
     elif 'Google' in x:
         A1 = sr.Recognizer()
-        url="https://www.google.com/search?q="
+        url = "https://www.google.com/search?q="
         with sr.Microphone() as  source:
             print("speak your qurries:")
             audio2 = A1.listen(source)
             try:
                 qurrie = A1.recognize_google(audio2)
                 print(qurrie)
-                wb.get().open_new(url+qurrie)
+                wb.get().open_new(url + qurrie)
 
             except:
                 print("Speak properly!!!!!")
@@ -68,16 +83,17 @@ def VoiceRecognition(x):
         A1 = sr.Recognizer()
         url = "https://www.google.com/search?q="
         try:
-            wb.get().open_new(url+x)
+            wb.get().open_new(url + x)
         except:
             print("speak Correctly......")
+
 
 try:
     with sr.Microphone() as source:
         print("speak searching plateform:")
         print("speak now:")
         print(datetime.datetime.now())
-        audio = A.listen(source,timeout=10 )
+        audio = A.listen(source, timeout=10)
         print(datetime.datetime.now())
         x = A.recognize_google(audio)
         print(x)
